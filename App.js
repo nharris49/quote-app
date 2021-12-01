0767ec912242c2fa09712888c118188c2e913259
nharris49 <nicholas.harris2020@gmail.com>
2021-12-01 01:52:37 -0500

    added background image, improved look

--- App.js
+++ App.js
@@ -1,39 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ImageBackground } from 'react-native';
+
 
 export default App = () =>
 {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
-  useEffect(() =>
+  function getQuote()
   {
     fetch('https://api.kanye.rest/')
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => console.error(error))
       .finally(() => setLoading(false))
+  }
+
+  useEffect(() =>
+  {
+    getQuote();
   }, []);
 
   return (
+    <ImageBackground source={require('./assets/kanye.png')} resizeMode="cover" style={styles.back}>
     <View style={styles.container}>
-      <Text fontSize={30}>
-        A Wise Man Once Said
-      </Text>
-      <Image source={require('./assets/head.gif')} />
-      <View style={styles.textBox} >
-        <Text style={styles.text}>{data.quote}</Text>
+        <Text style={{fontSize: 30, fontWeight: 'bold', color: 'white', paddingTop: 40}}>
+          A Wise Man Once Said
+        </Text>
+        <View style={styles.textBox} >
+          <Text style={styles.text}>{data.quote}</Text>
+        </View>
+        <View style={styles.buttonBox}>
+          <Button
+            title="Another One"
+            fontSize='bold'
+            onPress={() => getQuote()}
+          />
+        </View>
+        <StatusBar style="auto" />
       </View>
-      <View style={styles.buttonBox}>
-        <Button
-          title="Another One"
-          fontSize='bold'
-          onPress={() => useEffect()}
-        />
-      </View>
-      <StatusBar style="auto" />
-    </View>
+      </ImageBackground>
+
   );
 }
 
@@ -41,13 +49,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'space-around',
   },
-  image: {
-    paddingHorizontal: 50
-
+  back: {
+    flex: 1
   },
   textBox: {
     backgroundColor: '#ccc',
@@ -55,7 +61,7 @@ const styles = StyleSheet.create({
   },
   buttonBox: {
     backgroundColor: '#add8e6',
-    borderRadius: 10
+    borderRadius: 10,
   },
   text: {
     padding: 30,
